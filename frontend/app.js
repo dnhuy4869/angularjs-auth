0522c57f248286e1c7f77b3b7bddc82d51c57140
Huy <73681373+dnhuy4869@users.noreply.github.com>
2024-10-16 01:18:10 +0700

    login page

--- frontend/app.js
+++ frontend/app.js
@@ -3,5 +3,10 @@ var app = angular.module('myApp', []);
 app.controller('MainController', function ($scope) {
     $scope.title = "Welcome to My AngularJS Website!";
     $scope.description = "This is a simple single-page application using AngularJS.";
-    $scope.userName = "";
+
+    $scope.login = function () {
+
+        console.log($scope.username);
+        console.log($scope.password);
+    };
 });
